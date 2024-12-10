2f158dee617fd156b9d0d9ae10a1ef343e4f8df6
quillcraftsman <quill@craftsman.lol>
2024-12-10 11:14:47 +0300

    add is_modifiable function and unit tests

--- testing/conftest.py
+++ testing/conftest.py
@@ -2,6 +2,8 @@
 Pytest conftest file
 """
 import os
+import subprocess
+import tempfile
 from pytest import fixture
 
 
@@ -35,3 +37,45 @@ def log_to_clear_path(support_folder_path):
     # Cleanup after the test
     if os.path.exists(file_path):
         os.remove(file_path)
+
+
+@fixture
+def modifiable_file():
+    """
+    Return normal text file
+    """
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        yield tmp_file.name
+    os.unlink(tmp_file.name)  # Cleanup after the test
+
+
+@fixture
+def no_permission_file():
+    """
+    Return file without permissions on writing
+    """
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        file_path = tmp_file.name
+    os.chmod(file_path, 0o400)  # Read-only for the owner
+    yield file_path
+    os.chmod(file_path, 0o600)  # Restore permissions for cleanup
+    os.unlink(file_path)  # Cleanup
+
+
+@fixture
+def immutable_file():
+    """
+    Return immutable file with chattr +i option
+    """
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        file_path = tmp_file.name
+    try:
+        # Set immutable attribute
+        # subprocess.run(['sudo', 'chattr', '+i', file_path], check=True)
+        subprocess.run(['sudo', 'chattr', '+i', file_path], check=True)
+        yield file_path
+    finally:
+        # Remove immutable attribute and cleanup
+        # subprocess.run(['sudo', 'chattr', '-i', file_path], check=True)
+        subprocess.run(['sudo', 'chattr', '-i', file_path], check=True)
+        os.unlink(file_path)
