301034a8778888b637233abe04a69a5766de7390
quillcraftsman <quill@craftsman.lol>
2024-12-10 10:22:29 +0300

    add clear log function

--- quickstart/main.py
+++ quickstart/main.py
@@ -8,7 +8,8 @@ def main():
     TextLogCheck simple usage
     :return:
     """
-    from text_log_check import exists, get_tail_of_log  # pylint: disable=import-outside-toplevel
+    from text_log_check import exists, get_tail_of_log, clear_log  # pylint: disable=import-outside-toplevel
 
     print(exists('/var/log/auth.log'))
     print(get_tail_of_log('var/log/auth.log', 5))
+    clear_log('var/log/auth.log')
--- text_log_check/package.py
+++ text_log_check/package.py
@@ -2,5 +2,5 @@
 Package info
 """
 name = 'text-log-check'
-version = '0.0.4'
+version = '0.0.5'
 status = '3 - Alpha'
